9250efafb81adcf829e52a27c2f4ed7daaa61449
FluxOblivion <flux.oblivion@gmail.com>
2020-01-06 21:19:20 +1030

    Added fire particles
    Added fire particles and InteractFire script; the starting point for fire objects (next point collision detection for fire projectile?)

---
+++ Adventure Project/Assets/Scripts/InteractFire.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractFire : Interactable
+{
+    public ParticleSystem fireParticles;
+    public bool isLit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireParticles = GetComponent<ParticleSystem>();
+
+        if (!isLit)
+        {
+            fireParticles.Stop();
+        }else
+        {
+            fireParticles.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        if (fireParticles.isPlaying)
+        {
+            fireParticles.Stop();
+            isLit = false;
+        } else
+        {
+            fireParticles.Play();
+            isLit = true;
+        }
+    }
+}
