c27790accf566222923239dd4f610cd6bf7283ca
FluxOblivion <flux.oblivion@gmail.com>
2020-01-20 20:59:25 +1030

    Added sword to attack, among other things
    Sword will now appear during attack animation; wall texture has been adjusted along with some added wall skirtings... Might be other things, but forgot.

---
+++ Adventure Project/Assets/ToggleWeapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleWeapon : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject weapon;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            weapon.SetActive(true);
+        } else
+        {
+            weapon.SetActive(false);
+        }
+    }
+}
