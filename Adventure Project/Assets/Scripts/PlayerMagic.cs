c5f191a5bf829552221388cf05f3d52778a07016
FluxOblivion <flux.oblivion@gmail.com>
2020-01-22 21:36:44 +1030

    Changed out some walls, started on the projectile system
    Not a big change, looking after the kids again.

---
+++ Adventure Project/Assets/Scripts/PlayerMagic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMagic : MonoBehaviour
+{
+    public Transform shootPoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
