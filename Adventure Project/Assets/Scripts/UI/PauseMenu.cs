cb95f2b95f22372aa1fdcf509234c96ecdb75ec9
FluxOblivion <flux.oblivion@gmail.com>
2020-02-01 20:16:07 +1030

    Implemented Pause Menu
    Should help a lot with doing builds; being able to exit without forcibly closing. Should work on other menus next?

---
+++ Adventure Project/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        gamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("To be added soon!");
+
+        // Add when menu is actually implemented...
+        //Time.timeScale = 1f;
+        //SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game... Theoretically");
+
+        Application.Quit();
+    }
+}
